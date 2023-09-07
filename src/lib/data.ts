cd914e23635365a9b1ee83c6be41c067b0848492
Vasu Jhawar <vasujhawar2001@gmail.com>
2023-09-07 22:10:31 +0530

    init and header

---
+++ src/lib/data.ts
@@ -0,0 +1,69 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Blogs",
+    hash: "blogs",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Title2",
+    description:
+      "lorem Ipsum is Lorem Ipsum islorem Ipsum is Lorem Ipsum islorem Ipsum is Lorem Ipsum is",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind"],
+    imageUrl: "",
+  },
+  {
+    title: "Title2",
+    description:
+      "Jlorem Ipsum is Lorem Ipsum islorem Ipsum is Lorem Ipsum islorem Ipsum is Lorem Ipsum is",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind"],
+    imageUrl: "",
+  },
+  {
+    title: "Title3",
+    description:
+      "lorem Ipsum is Lorem Ipsum islorem Ipsum is Lorem Ipsum islorem Ipsum is Lorem Ipsum is",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind"],
+    imageUrl: "",
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Express",
+  "PostgreSQL",
+  "Framer Motion",
+] as const;
