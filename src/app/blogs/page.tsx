28d2a7924f08fd3ebc1b813783d94ab754886cad
Vasu Jhawar <vasujhawar2001@gmail.com>
2023-09-11 01:22:57 +0530

    deployed

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -8,7 +8,7 @@ type Props = {}
 const page = (props: Props) => {
   return (
     <div className='bg-gradient-to-r from-fuchsia-500 to-pink-500 bg-clip-text hover:text-transparent
-    sm:text-5xl text-3xl font-extrabold flex h-full items-center justify-center mt-96'>
+    sm:text-5xl text-3xl font-extrabold flex h-full items-center justify-center min-h-screen'>
             <Typewriter
             words={["Coming Soon...", "Stay Tuned!", "<//Work in Progress//>"]}
             loop={true}
